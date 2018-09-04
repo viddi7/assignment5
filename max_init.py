eea143f1d67fce7a1e4f74528280ebe63fd93b3c
viddi7 <vidar-oli@hotmail.com>
2018-09-04 10:30:01 +0000

    first assigntment

--- max_init.py
+++ max_init.py
@@ -1,1 +1,7 @@
-print("Hello")
+num_int = int(input("Input a number: "))    # Do not change this line
+# Fill in the missing code
+
+
+
+
+print("The maximum is", max_int)    # Do not change this line
