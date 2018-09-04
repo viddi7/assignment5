f613e3c5052b8040181ed5fe512421575f5bf374
viddi7 <vidar-oli@hotmail.com>
2018-09-04 10:46:49 +0000

    commitement

--- max_init.py
+++ max_init.py
@@ -1,6 +1,15 @@
 num_int = int(input("Input a number: "))    # Do not change this line
 # Fill in the missing code
 
+num_int = 0
+max_int = 0
+
+while num_int >= 0:
+    num_int = int(input("Input a number: "))
+    if num_int > max_int:
+        max_int = num_int
+    
+
 
 
 
